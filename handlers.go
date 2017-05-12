8e91eda6f8c34b2c532dbf0719e74f40e9fe89ad
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-05-12 13:14:43 +0400

    new revenue struct in reporter

--- handlers.go
+++ handlers.go
@@ -71,17 +71,22 @@ func GetNewBlackListed(providerName string, time string) ([]string, error) {
 
 func GetRandomAggregate() acceptor.Aggregate {
 	return acceptor.Aggregate{
-		ReportAt:     time.Now().UTC().Unix(),
-		CampaignId:   rand.Int63n(9),
-		ProviderName: "cheese",
-		OperatorCode: 52000,
-		LpHits:       rand.Int63n(200),
-		LpMsisdnHits: rand.Int63n(150),
-		Mo:           rand.Int63n(200),
-		MoUniq:       rand.Int63n(200),
-		MoSuccess:    rand.Int63n(150),
-		RetrySuccess: rand.Int63n(150),
-		Pixels:       rand.Int63n(200),
+		ReportAt:             time.Now().UTC().Unix(),
+		CampaignId:           rand.Int63n(9),
+		ProviderName:         "cheese",
+		OperatorCode:         52000,
+		LpHits:               rand.Int63n(200),
+		LpMsisdnHits:         rand.Int63n(150),
+		MoTotal:              rand.Int63n(200),
+		MoChargeSuccess:      rand.Int63n(200),
+		MoChargeSum:          1000.,
+		MoChargeFailed:       rand.Int63n(200),
+		MoRejected:           rand.Int63n(200),
+		RenewalTotal:         rand.Int63n(200),
+		RenewalChargeSuccess: rand.Int63n(200),
+		RenewalChargeSum:     12312.,
+		RenewalFailed:        rand.Int63n(200),
+		Pixels:               rand.Int63n(200),
 	}
 }
 
